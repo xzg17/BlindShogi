f07e99db39c08a647d5a36d1f0201ef51ae2ceed
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-27 11:11:22 +0900

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -12,7 +12,7 @@ typedef struct {
 } Py_Test_Class;
 
 
-static PyObject *test_func(Py_Test_Class *self, PyObject *args);
+static PyObject *test_func(Py_Test_Class *self);
 
 static int test_init(Py_Test_Class *self, PyObject *args);
 
